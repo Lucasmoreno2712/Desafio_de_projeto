e6a706dffd297b14e423b6411c80238ed6a73ace
lucasmoreno2712 <lucasmorenolopes02@gmail.com>
2021-12-19 13:44:31 -0300

    curso logica de programação

---
+++ curso logica de programação/primeiro/primeiro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace programa
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            double salario;
+            double altura;
+            char sexo;
+            string nome;
+
+            idade = 20;
+            salario = 2000.00;
+            altura = 1.70;
+            sexo = 'M';
+            nome = "lucas moreno";
+            Console.WriteLine(idade);
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(altura);
+            Console.Write(sexo);
+        }
+    }
+}
