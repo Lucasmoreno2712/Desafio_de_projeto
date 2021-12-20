2407921cab6e991bc212b29d57c0ae06cff1d605
lucasmoreno2712 <lucasmorenolopes02@gmail.com>
2021-12-19 21:52:52 -0300

    curso logica de programação

---
+++ curso logica de programação/primeiro/Interface1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    interface IAnimal
+    {
+        void Nome(string name);
+        void NomeDono(string nomedono);
+        void Especie(string especie);
+    }
+    class animal : IAnimal
+    {
+        public string nome { get; set; }
+        public string NomeDono { get; set; }
+        public string Especie { get; set; }
+        public string Nome { get; private set; }
+
+        void IAnimal.Nome(string nome)
+        {
+            this.Nome = nome;
+        }
+        void IAnimal.NomeDono(string nomedono)
+        {
+            this.NomeDono = nomedono;
+        }
+        void IAnimal.Especie(string especie)
+        {
+            this.Especie = especie;
+        }
+    } 
+}
